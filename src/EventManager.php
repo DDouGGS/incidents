b1893ae033ff1ca741cf265ae20f07ab94eee56d
Douglas Gonçalves <douglas.goncalves@grupofolha.com.br>
2026-04-06 08:56:32 -0300

    Muda classe paa EventManager

---
+++ src/EventManager.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ddouggs\event_manager;
+
+use ddouggs\event_manager\Event;
+
+abstract class EventManager
+{
+    public static $events = array();
+
+    // Registra evento
+    // public static function register(string $name, $event)
+    // {
+    //     \test_command\TC::halt(array($name, $event));
+    //     if (isset($name) && !empty($name)) {
+    //         self::$events[$name] = $event;
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // Acessa evento
+    // public static function access(string $name)
+    // {
+    //     if (isset(self::$events[$name])) {
+    //         return self::$events[$name];
+    //     }
+    //     return null;
+    // }
+
+    // Acessa evento
+    public static function exists(string $name)
+    {
+        return (isset(self::$events[$name]) && !empty(self::$events[$name]))? true: false;
+    }
+}
